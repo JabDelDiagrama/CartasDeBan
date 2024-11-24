5c416e29ddfc1b3a22d7ebd7b6f1b9612362cb34
Jab del Diagrama <jabdeldiagrama0000@gmail.com>
2024-11-23 22:13:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,30 @@ const cardContainer = document.getElementById("card-container");
 // Renderiza las cartas en el DOM
 function renderCards(cards) {
     cards.forEach(card => {
+        let decoracion = ""
+        if(card?.natEvent){
+            decoracion = "<div class='widt100'><img src='./asset/flor.png'></div>"
+        }
         const cardElement = document.createElement("div");
         cardElement.classList.add("card");
 
         cardElement.innerHTML = `
-            <h2>Código: ${card.code}</h2>
-            <p><strong>Descripción:</strong> ${card.description}</p>
-            <p><strong>Comentario extra:</strong> ${card.statBroma}</p>
-            
-            <p><strong>Estado:</strong> ${card.status}</p>
-            <p><strong>Razon de Expulsion:</strong> ${card.razonExpulsion}</p>
-            <p><strong>Errores del servidor:</strong> ${card.serverErrors}</p>
-            <p><strong>Sanciones:</strong></p>
-            <ul>${card.sanctions.map(s => `<li>${s}</li>`).join("")}</ul>
-        `;
+            <div class="card_header">
+                <h2>Nombre en Clave: ${card.code}</h2>
+            </div>
+            <div class="card_body">
+                <p><strong>Descripción:</strong> ${card.description}</p>
+                <p><strong>Comentario extra:</strong> ${card.statBroma}</p>
+                
+                <p><strong>Estado:</strong> ${card.status}</p>
+                <p><strong>Razon de Expulsion:</strong> ${card.razonExpulsion}</p>
+                <p><strong>Errores del servidor:</strong> ${card.serverErrors}</p>
+                <p><strong>Sanciones:</strong></p>
+                <ul>${card.sanctions.map(s => `<li>${s}</li>`).join("")}</ul>
+                ${decoracion}
+                
+            </div>
+            `;
 
         cardContainer.appendChild(cardElement);
     });
